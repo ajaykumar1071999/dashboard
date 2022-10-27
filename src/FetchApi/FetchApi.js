b4ac0761cb6f7693312e21ac04f1f2ac31f7e392
Ajay Kumar <ajaykumar1071999@gmail.com>
2022-10-27 18:35:39 +0530

    Edit & Update Data

--- src/FetchApi/FetchApi.js
+++ src/FetchApi/FetchApi.js
@@ -1,21 +1,28 @@
 import "./FetchApi.css";
 import { useState, useEffect } from "react";
 function FetchApi() {
-  const [user, setUser] = useState([]);
-  const [name,setName]=useState("");
-  const [email,setEmail]=useState("");
-  const [mobile,setMobile]=useState("");
+    const [user, setUser] = useState([])
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
   const [dob,setDob]=useState("");
+  const [userId,setUserId]=useState(null)
+  
 
   useEffect(() => {
     fetchData();
   }, []);
-  function fetchData() {
+  function fetchData(id) {
     fetch("http://localhost:3004/user")
       .then((result) => result.json())
       .then((resp) => {
         console.log(resp);
         setUser(resp);
+        setName(resp[0].name)
+        setEmail(resp[0].email)
+        setMobile(resp[0].mobile)
+        setDob(resp[0].dob)
+        setUserId(resp[0].id)
         
         
       });
@@ -32,17 +39,37 @@ function FetchApi() {
     });
     fetchData();
   }
-  function updateData(id)
-  {
-    let items=user[id-1];
-    console.log(user[id-1])
-        setName(items.name)
-        setEmail(items.email)
-        setMobile(items.mobile);
-        setDob(items.dob)
+  function selectUser(id){
+    let items=user[id-1]
+    console.log(items)
+    setName(items.name)
+    setEmail(items.email)
+    setMobile(items.mobile)
+    setDob(items.dob)
+    setUserId(items.id)
+
   }
 
+function updateUser(){
+    let item1={userId,name,email,mobile,dob}
+
+    fetch(`http://localhost:3004/user/${userId}`, {
+      method: "PUT",
+      headers:{
+        'Accept':'application.json',
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(item1)
+    }).then((result) => {
+      result.json().then((resp) => {
+        console.log(resp);
+      })
+    });
+    fetchData();
+    
+}
   return (
+    
     <>
       <div>
         <table style={{ width: "100%" }}>
@@ -58,7 +85,7 @@ function FetchApi() {
 
           {user.map((item, i) => (
             <tr key={i}>
-              <td>{item.id-1}</td>
+              <td>{item.id}</td>
               <td>{item.name}</td>
               <td>{item.email}</td>
               <td>{item.mobile}</td>
@@ -71,21 +98,24 @@ function FetchApi() {
                   Delete
                 </button>
               </td>
-              <td><button
-                  className="btn btn-outline-warning form-control" onClick={(id) => updateData(item.id)}
+              <td>
+                <button
+                  className="btn btn-outline-danger form-control"
+                  onClick={(id) => selectUser(item.id)}
                 >
-                  Update
-                </button></td>
+                  Edit
+                </button>
+              </td>
             </tr>
           ))}
         </table>
 
-        <div>
-          <input type="text" name="name" className="form-control offset-5" value={name}/>
-          <input type="text" name="email" className="form-control offset-5" value={email} />
-          <input type="text" name="mobile" className="form-control offset-5" value={mobile} />
-          <input type="text" name="dob" className="form-control offset-5" value={dob}/>
-          <button className="btn btn-outline-success form-control offset-5">Update</button>
+        <div className="col-sm-6 offset-sm-3 ">
+          <input type="text" name="name" className="form-control" value={name}  onChange={(e)=>setName(e.target.value)}/>
+          <input type="text" name="email" className="form-control" value={email}   onChange={(e)=>setEmail(e.target.value)}  />
+          <input type="text" name="mobile" className="form-control"  value={mobile}  onChange={(e)=>setMobile(e.target.value)} />
+          <input type="text" name="dob" className="form-control " value={dob} onChange={(e)=>setDob(e.target.value)}  />
+          <button className="btn btn-outline-success form-control" onClick={updateUser}>Update</button>
         </div>
       </div>
     </>
